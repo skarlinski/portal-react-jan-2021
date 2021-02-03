e9837aeba5a37779a1f057082085f4ad575f42c0
shai33 <shaib3@live.com>
2021-02-03 14:31:35 +0200

    add class PortalTable

---
+++ src/components/PortalTable.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PortalTable extends React.Component{
+    constructor(props){
+        super(props);
+        
+    }
+    render(){
+    return (
+        <div className="c-portalTable">
+            
+        </div>
+    );
+    }
+}
+
+export default CoursesPage;
