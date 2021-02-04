c24adcefa7ce7621042c4ec069e083b1f81a9ea0
skarlinski <skarlinski@gmail.com>
2021-02-04 13:17:51 +0200

    revert to courses in fucntional syntax

--- src/pages/courses/CoursesPage.js
+++ src/pages/courses/CoursesPage.js
@@ -3,38 +3,21 @@ import './courses.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
-import server from '../../shared/server';
-import { act } from 'react-dom/test-utils';
 
-class CoursesPage extends React.Component{
-    constructor(props){
-        super(props);
-        this.activeUser = {token: '1304918f47c5f86e54935c4bed5563afbfd0c'};
-        this.state = {
-            courses: []
-        }
-    }
-    componentDidMount(){
-        server(this.activeUser, {"search":"","sorting":"courseid","desc":false,"coursestatus":1,"page":0}, 'SearchCourses').then( res => {
-            console.log(res)
-            this.setState({courses: res.data.courses}) 
-        })
-    }
-    render(){
-    if (!this.activeUser) {
+const CoursesPage = (props) => {
+    const { handleLogout } = props;
+    const activeUser = useContext(ActiveUserContext);
+    
+    if (!activeUser) {
         return <Redirect to='/' />
     }
-    const coursesList = this.state.courses.map( (course) => {
-        return <div>{course.name} - {course.subname} - {course.year}</div>
-    } )
+
     return (
         <div className="p-courses">
-            <PortalNavbar handleLogout={this.props.handleLogout}/>
+            <PortalNavbar handleLogout={handleLogout}/>
             <h1>קורסים</h1>
-            {coursesList}
         </div>
     );
-    }
 }
 
 export default CoursesPage;
