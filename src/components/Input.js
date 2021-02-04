a4e48807b1fdef09c3a755e9c2c789abbef0b247
Shenhav Topaz <shenhavtop@hotmail.com>
2021-02-04 14:22:50 +0200

    remove comments

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,12 +2,6 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import './Input.css'
 
- 
-// Props:
-// - title. string. if missing not rendering a title and the component will be shorter.
-// - placeholder. string.
-// - handleChange. callback function. invoked when the input changes. sends the new text.
-
 class Input extends React.Component{
     constructor(props){
         super(props);
