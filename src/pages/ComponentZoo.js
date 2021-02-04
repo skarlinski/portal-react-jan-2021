ad1c9a0cd0953c6eb2fda416eed8ed61d1c6d78e
Shenhav Topaz <shenhavtop@hotmail.com>
2021-02-04 14:08:00 +0200

    add new input component

--- src/pages/ComponentZoo.js
+++ src/pages/ComponentZoo.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Input from '../components/Input';
+
+
+
+// Props:
+// - title. string. if missing not rendering a title and the component will be shorter.
+// - placeholder. string.
+// - handleChange. callback function. invoked when the input changes. sends the new text.
+
 
 class ComponentZoo extends React.Component{
+    handleChange = (e)=>{
+        console.log(e);
+    }
 
     render(){
+        const   title = "מס טלפון";
+        const placeholder = "input-placeholder";
         return <div>
            myzoo
+           <Input title={title} placeholder={placeholder} handleChange={this.handleChange} />
+           <Input title={""} placeholder={placeholder} handleChange={this.handleChange} />
+
         </div>
     }
 }
