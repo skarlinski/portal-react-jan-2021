4f2a32e4831805241e31860cf66c7cd38d56334e
skarlinski <skarlinski@gmail.com>
2021-02-07 10:24:48 +0200

    add props in zoo

--- src/pages/ComponentZoo.js
+++ src/pages/ComponentZoo.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PortalSearchBar from '../components/searchbar/PortalSearchBar';
 
 class ComponentZoo extends React.Component{
+    constructor(props){
+        this.state = {
+            searchText:''
+        }
+    }
     handleSearch = (text) => console.log('the text is' + text);
     pageChange = (page) => console.log(page);
-
+    
     render(){
         return <div>
            <PortalSearchBar handleSearch={this.handleSearch}
+            searchText={this.state.searchText}
+            handleSearch={this.handleSearch}
             placeholderText="placeholder Text" resPageNum={3} 
             currentPage={2} pageChange={this.pageChange}/>
         </div>
