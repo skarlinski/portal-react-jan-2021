c15b0caa1efea7eba3c5e55e24dcd93d9c5580ff
Tamar Cohen Yogev <cohen.tamar@gmail.com>
2021-02-09 12:45:21 +0200

    basic

--- src/pages/users/UsersPage.js
+++ src/pages/users/UsersPage.js
@@ -1,22 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './users.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
 import profileIcon from './profile_icon.svg'
+import PortalTable from '../../components/PortalTable';
+//import server
+//import portal table
+
 const UsersPage = (props) => {
     const { handleLogout } = props;
     const activeUser = useContext(ActiveUserContext);
+    const [users , setUsers] = useState([]);
+
+    //read data from api using server function
     
     if (!activeUser) {
         return <Redirect to='/' />
     }
     
+    const handleClick = (data) => {
+        console.log(data);
+    };
+
+    const headers = [
+        {
+            header:'שם',
+            key:''
+        },
+        {
+            header:'שם משפחה',
+            key:''
+        },
+        {
+            header:'אימייל',
+            key:''
+        }
+    
+    
+    ]
+
+    //create array with the data from the sate 
+    const usersList = []
+
     return (
         <div className="p-users">
             <PortalNavbar handleLogout={handleLogout}/>
-            <h1>משתמשים</h1>
-            <img src={profileIcon}/> 
+            <h1>עובדים</h1>
+            {/* <PortalSearchBar handleSearch={this.handleSearch}
+            searchText={this.state.searchText}
+            handleSearch={this.handleSearch}
+            pageChange={this.pageChange}
+            placeholderText="placeholder Text" resPageNum={this.state.resPageNum} 
+            currentPage={this.state.currentPage} /> */}
+            {usersList? <PortalTable headers={headers} data={usersList}  handleClick={handleClick}/>: ''}
+            {/* <PortalButtonSet/> */}
         </div>
     );
 }
