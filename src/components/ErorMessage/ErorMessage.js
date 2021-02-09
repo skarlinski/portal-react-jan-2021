0e2d4f0ec2aead6ad7c3a79f0a43de762b16f7b6
Yogev Etz Haim <yogev76@gmail.com>
2021-02-09 10:56:09 +0200

    fix & clean

--- src/components/ErorMessage/ErorMessage.js
+++ src/components/ErorMessage/ErorMessage.js
@@ -2,22 +2,16 @@ import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom';
 import './errormessage.css'
-// import warnnigSign from './noun_error_1156903.svg';
 
 class ErrorMessage extends React.Component {
     constructor(props){
         super(props)
         
     }
-
-    alertx() {
-        console.log("hello");
-    }
    
     render() {
         const style={color : this.props.txtColor,  backgroundColor : this.props.bgColor} 
         return(
-        //    <div className="c-errormessage"> 
                 <Container className="c-errormessage" style={style}>
                     <Row>
                         <Col>
@@ -26,15 +20,11 @@ class ErrorMessage extends React.Component {
                         <Col>
                         {this.props.errMsg}
                         </Col>
-                        <Col className="top-left" onClick={this.props.handleClose} >
-                           
-                            x
-                           
+                        <Col className="top-left" onClick={this.props.handleClose} >         
+                            x             
                         </Col>
                     </Row>
-                {/* {this.props.msg} */}
                 </Container>
-            // </div>
         )
     }
 }
