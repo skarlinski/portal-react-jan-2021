bdf2a010453e3bb83bd0caf0d940ad11512592e1
shai33 <shaib3@live.com>
2021-02-11 14:51:57 +0200

    CoursePage - Add the search bar functionality

--- src/pages/courses/CoursesPage.js
+++ src/pages/courses/CoursesPage.js
@@ -15,15 +15,17 @@ const CoursesPage = (props) => {
     const [searchText , setText] = useState('');
     const [currentPage , setCurrPage] = useState(0);
     const [resPageNum , setPageNum] = useState(0);
+    const [courseStatus , setCourseStatus] = useState(1);
 
     useEffect(() => {
-        server(activeUser, {"search": "", "sorting": "courseid", "desc": false, "coursestatus": 1,
-        "page": 0}, 'SearchCourses').then( res => {
+        server(activeUser, {"search": searchText, "sorting": "courseid", "desc": false, 
+        "coursestatus": courseStatus, "page": currentPage}, 'SearchCourses').then( res => {
             console.log(res)
             console.log(res.data.courses)
-            setCourses({courses: res.data.courses}) 
+            setCourses({courses: res.data.courses});
+            setPageNum(res.data.pages);
         });
-    }, []);
+    }, [searchText, courseStatus, currentPage]);
 
     if (!activeUser) {
         return <Redirect to='/' />
@@ -34,27 +36,33 @@ const CoursesPage = (props) => {
         // return <Redirect push to="/#/courses/data.courseid" />
         window.location = "/#/courses/" + data.courseid;
     };
+    // const clickCoursestatus = (data) => {
+    //     console.log(data);
+    //     if(data.key === "off"){
+    //         server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
+    //         "coursestatus": 0, "page": 0}, 'SearchCourses').then( res => {
+    //             setCourses({courses: res.data.courses}) 
+    //         });
+    //     }
+    //     else{
+    //         server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
+    //         "coursestatus": 1, "page": 0}, 'SearchCourses').then( res => {
+    //             setCourses({courses: res.data.courses}) 
+    //         });
+    //     }
+    // };
     const clickCoursestatus = (data) => {
-        console.log(data);
-        if(data.key === "off"){
-            server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
-            "coursestatus": 0, "page": 0}, 'SearchCourses').then( res => {
-                setCourses({courses: res.data.courses}) 
-            });
-        }
-        else{
-            server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
-            "coursestatus": 1, "page": 0}, 'SearchCourses').then( res => {
-                setCourses({courses: res.data.courses}) 
-            });
-        }
+        setCurrPage(0);
+        const keyVar = (data.key === "off")? 0 : 1;
+        setCourseStatus(keyVar);
     };
-    
-    const handleSearch = (text) => console.log('the text is' + text);
+    const handleSearch = (text) => {
+        setCurrPage(0);
+        setText(text);
+    };
     const pageChange = (page) => {
-        this.setState({currentPage: page})
-        console.log(page);
-    }
+        setCurrPage(page);
+    };
     const headers = 
         [
             {
@@ -75,6 +83,7 @@ const CoursesPage = (props) => {
                 {key:'on', label:'קורסים פעילים'},
                 {key:'off', label:'קורסים לא פעילים'},
             ];
+        const placeholderText = "חיפוש קורס";
         // const data =
         //   [
         //     {
@@ -96,9 +105,12 @@ const CoursesPage = (props) => {
             <PortalNavbar handleLogout={handleLogout}/>
             <h1>קורסים</h1>
             <div className="search-courses">
-                <PortalSearchBar handleSearch={handleSearch} searchText={searchText}
-                    pageChange={pageChange} placeholderText="חיפוש קורס" 
-                    resPageNum={resPageNum} currentPage={currentPage} />
+            <PortalSearchBar handleSearch={handleSearch}
+                searchText={searchText}
+                handleSearch={handleSearch}
+                pageChange={pageChange}
+                placeholderText={placeholderText} resPageNum={resPageNum} 
+                currentPage={currentPage} />
             </div>
             <div className="l-courses">
                 {coursesList ? <PortalTable headers={headers} data={coursesList} 
