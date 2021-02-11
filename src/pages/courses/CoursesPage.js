5c64aa04a31eaeaecfa23798c2d20b36708dbc8f
Tamar Cohen Yogev <cohen.tamar@gmail.com>
2021-02-11 14:18:35 +0200

    with Tamars changes

--- src/pages/courses/CoursesPage.js
+++ src/pages/courses/CoursesPage.js
@@ -15,46 +15,46 @@ const CoursesPage = (props) => {
     const [searchText , setText] = useState('');
     const [currentPage , setCurrPage] = useState(0);
     const [resPageNum , setPageNum] = useState(0);
+    const [courseStatus , setCoursesStatus] = useState(0);
+
+
 
     useEffect(() => {
-        server(activeUser, {"search": "", "sorting": "courseid", "desc": false, "coursestatus": 1,
-        "page": 0}, 'SearchCourses').then( res => {
-            console.log(res)
-            console.log(res.data.courses)
-            setCourses({courses: res.data.courses}) 
+        server(activeUser, {"search": searchText, "sorting": "courseid", "desc": false, "coursestatus": courseStatus,
+        "page": currentPage}, 'SearchCourses').then( res => {
+            setText(searchText);
+            setCurrPage(currentPage);
+            setPageNum(resPageNum);
+            setCourses(res.data.courses);
+            setCoursesStatus(courseStatus); 
         });
-    }, []);
+    }, [currentPage, searchText, courseStatus]);
 
     if (!activeUser) {
         return <Redirect to='/' />
     };
     const handleClick = (data) => {
-        console.log(data);
-        console.log(data.courseid);
-        // return <Redirect push to="/#/courses/data.courseid" />
+                // return <Redirect push to="/#/courses/data.courseid" />
         window.location = "/#/courses/" + data.courseid;
     };
+
     const clickCoursestatus = (data) => {
-        console.log(data);
-        if(data.key === "off"){
-            server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
-            "coursestatus": 0, "page": 0}, 'SearchCourses').then( res => {
-                setCourses({courses: res.data.courses}) 
-            });
-        }
-        else{
-            server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
-            "coursestatus": 1, "page": 0}, 'SearchCourses').then( res => {
-                setCourses({courses: res.data.courses}) 
-            });
-        }
-    };
-    
-    const handleSearch = (text) => console.log('the text is' + text);
-    const pageChange = (page) => {
-        this.setState({currentPage: page})
-        console.log(page);
+        setCurrPage(0);
+        const key = (data.key == "off")? 0 : 1;
+        setCoursesStatus(key);
+
     }
+
+    const handleSearch = (text) => {
+        setCurrPage(0);
+        setText(text);
+    }
+        
+    const pageChange = (page) => {
+        setCurrPage(page);
+    }
+
+
     const headers = 
         [
             {
@@ -70,7 +70,8 @@ const CoursesPage = (props) => {
                 key: 'teachers'
             }
         ];
-        const buttons =
+
+    const buttons =
             [
                 {key:'on', label:'קורסים פעילים'},
                 {key:'off', label:'קורסים לא פעילים'},
@@ -90,7 +91,8 @@ const CoursesPage = (props) => {
 		// 		year: "2017"
         //     }
 		//   ];
-        const coursesList = courses.courses
+    const coursesList = courses.courses
+
     return (
         <div className="p-courses">
             <PortalNavbar handleLogout={handleLogout}/>
