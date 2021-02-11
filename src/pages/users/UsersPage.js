6d877595b0dc73c0b431f7e093f18a88a73fec2f
Tamar Cohen Yogev <cohen.tamar@gmail.com>
2021-02-11 12:31:04 +0200

    bug with the table

--- src/pages/users/UsersPage.js
+++ src/pages/users/UsersPage.js
@@ -3,7 +3,6 @@ import './users.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
-import profileIcon from './profile_icon.svg'
 import PortalTable from '../../components/PortalTable';
 import ButtonSet from '../../components/ButtonSet';
 import server from '../../shared/server';
@@ -18,84 +17,36 @@ const UsersPage = (props) => {
     const [searchText , setText] = useState('');
     const [currentPage , setCurrPage] = useState(0);
     const [resPageNum , setPageNum] = useState(0);
+    const [userStatus , setUserStatus] = useState(0);
 
 
-
-
-    //read data from api using server function
-
-    useEffect(() => {
-    server(activeUser, {"search":"","sorting":"userid","desc":false,"userstatus":1,"page":0,"v":2.3} ,'SearchStaffUnderMe').then(res => {
-    
-        setPageNum({resPageNum:res.data.pages});
-        setUsers({users:res.data});
+useEffect(() => {
+    server(activeUser, {"search":searchText,"sorting":"userid","desc":false,"userstatus":userStatus,"page":currentPage} ,'SearchStaffUnderMe').then(res => {
+        setPageNum(res.data.pages);
+        setUsers(res.data.users);
+        console.log(res.data.users);
     })
-},[]);
+},[searchText, userStatus, currentPage]);
     
-
     if (!activeUser) {
         return <Redirect to='/' />
     }
+
     
-    const handleClick = (data) => {
-        console.log(data);
-    };
-
-
-    //gets the wanted search text (a string) from the search-bar module 
-    // and makes an ajax call using the text string  (in the "search" key) and with "0" in the "pages" key
     const handleSearch = (text) => {
-        server(activeUser, {"search":text,"sorting":"userid","desc":false,"userstatus":1,"page":0,"v":2.3} ,'SearchStaffUnderMe').then(res => {   
-            
-            setPageNum({resPageNum:res.data.pages});
-            setUsers({users:res.data});
-        })
-
-        console.log('the text is' + text);}
-
-
-    //1. gets the wanted page num (integer) from the search-bar module (the currnet num +/- 1) and makes an ajax call 
-    //    using the new page number (in the "pages" key)
-    //2. update the new page number in the "currentPage" variable in the state
+        setText(text);
+    }
 
     const pageChange = (page) => {
-        server(activeUser, {"search":"","sorting":"userid","desc":false,"userstatus":1,"page":page,"v":2.3} ,'SearchStaffUnderMe').then(res => {
-            setCurrPage({currentPage:page});
-            setUsers({users:res.data});
-            console.log(currentPage);
-        })
-        
-        
-
-
-    }
-
+            setCurrPage(page);
+        }
+          
     const clickUserStatus = (data) => {
-        console.log(data);
-        if(data.key === "off"){
-            server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
-            "userstatus": 0, "page": 0}, 'SearchCourses').then( res => {
-                setUsers({users: res.data.users}) 
-            });
-        }
-        else{
-            server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
-            "userstatus": 1, "page": 0}, 'SearchCourses').then( res => {
-                setUsers({users: res.data.users}) 
-            });
-        }
-        console.log(users)
-    };
-
-    const temp = (data) => {
-        let keyVar;
-        (data.key === "off")? keyVar=0 : keyVar=1;
-        server(activeUser, {"search": "", "sorting": "courseid", "desc": false, 
-        "userstatus": keyVar, "page": 0}, 'SearchCourses').then( res => {
-            setUsers({users: res.data.users}) 
-        });
-
+        let keyVar = (data.key === "off")? 0 : 1;
+        setUserStatus(keyVar);
     }
+
+       
 
     const headers = [
         {
@@ -110,21 +61,14 @@ const UsersPage = (props) => {
             header:'אימייל',
             key:'email'
         }
-    
-    
     ]
 
     const placeholderText = "חיפוש עובד";
 
     const buttons = [{key:'on', label:'עובדים  פעילים'},{key:'off', label:'עובדים לא פעילים'}];
 
-    // const usersList = users.users.users;
+    const usersList = users.users;
 
-    
-
-    
-
-    
 
     return (
         <div className="p-users">
@@ -135,9 +79,7 @@ const UsersPage = (props) => {
             pageChange={pageChange}
             placeholderText={placeholderText} resPageNum={resPageNum} 
             currentPage={currentPage} />
-            {/* {usersList? <PortalTable headers={headers} data={usersList}  handleClick={handleClick}/>: ''} */}
-            
-
+            {usersList? <PortalTable headers={headers} data={usersList}/>: ''}
             <ButtonSet buttons={buttons} handleClick={clickUserStatus}/>
         </div>
     );
