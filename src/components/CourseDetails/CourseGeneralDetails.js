26d4385cc0e1b8e25273c2a2bbab5d8e256261e3
skarlinski <skarlinski@gmail.com>
2021-02-11 13:24:54 +0200

    Handover code to Yogev & shenhav

--- src/components/CourseDetails/CourseGeneralDetails.js
+++ src/components/CourseDetails/CourseGeneralDetails.js
@@ -4,6 +4,7 @@ import Input from "../Input";
 const CourseGeneralDetails = function(props){
     const {courseData} = props;
     const [fullName, setFullName] = useState('');
+    console.log(courseData)
     return (
         <div className="p-course-general-details">
             <Input title="שם קורס מלא" value={fullName || courseData.name} handleChange={setFullName}/>
